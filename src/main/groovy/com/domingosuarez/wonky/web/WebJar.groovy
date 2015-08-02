67534c0de542cf80d1d4cebe804003d42b1d617f
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-08-02 15:08:10 -0500

    extracted the css, added support for webjars-locator

---
+++ src/main/groovy/com/domingosuarez/wonky/web/WebJar.groovy
@@ -0,0 +1,35 @@
+package com.domingosuarez.wonky.web
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.servlet.HandlerMapping
+import org.webjars.WebJarAssetLocator
+
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Created by domix on 02/08/15.
+ */
+@Controller
+class WebJar {
+
+  @ResponseBody
+  @RequestMapping("/webjarslocator/{webjar}/**")
+  public ResponseEntity locateWebjarAsset(@PathVariable String webjar, HttpServletRequest request) {
+    WebJarAssetLocator assetLocator = new WebJarAssetLocator()
+    try {
+      String mvcPrefix = "/webjarslocator/" + webjar + "/"; // This prefix must match the mapping path!
+      String mvcPath = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+      String fullPath = assetLocator.getFullPath(webjar, mvcPath.substring(mvcPrefix.length()));
+      return new ResponseEntity(new ClassPathResource(fullPath), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  }
+}
