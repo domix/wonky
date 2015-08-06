c781b22176c8d3d9917f9fe3e6cfc0b61773e59b
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-08-05 23:56:25 -0500

    fiish first iteration

--- src/main/groovy/com/domingosuarez/wonky/service/SlackService.groovy
+++ src/main/groovy/com/domingosuarez/wonky/service/SlackService.groovy
@@ -1,11 +1,29 @@
+/*
+ *
+ * Copyright (C) 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.domingosuarez.wonky.service
 
-import groovy.json.JsonSlurper
+import static org.springframework.context.i18n.LocaleContextHolder.locale
+
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.context.MessageSource
 import org.springframework.stereotype.Service
 import wslite.rest.RESTClient
-import wslite.rest.Response
 
 /**
  * Created by domix on 19/07/15.
@@ -13,18 +31,28 @@ import wslite.rest.Response
 @Service
 @Slf4j
 class SlackService {
-  Map slack(String token, String host) {
-    RESTClient client = new RESTClient("https://${host}.slack.com/api")
-    Response response = client.get(path: '/rtm.start', query: [token: token])
+  @Autowired
+  MessageSource messageSource
 
-    new JsonSlurper().parseText(response.contentAsString)
+  Map slack(String token, String host) {
+    new RESTClient("https://${host}.slack.com/api")
+      .get(path: '/rtm.start', query: [token: token])
+      .parsedResponseContent.json
   }
 
-  String invite(String token, String host, String email) {
-    new RESTClient("https://${host}.slack.com/api/users.admin.invite").post {
+  Map invite(String token, String host, String email) {
+    Map response = new RESTClient("https://${host}.slack.com/api/users.admin.invite").post {
       charset 'UTF-8'
       urlenc token: token, email: email
-    }.contentAsString
+    }.parsedResponseContent.json
+
+    if (response.ok) {
+      response.message = messageSource.getMessage('invite.success', [].toArray(), 'WOOT. Check your email!', locale)
+    } else {
+      response.error = messageSource.getMessage(response.error, [].toArray(), response.error, locale)
+    }
+
+    response
   }
 
   @Cacheable('slackPublicData')
--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,3 +1,19 @@
+/*
+ *
+ * Copyright (C) 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 scan '30 seconds'
 
 String encoderPattern = '%-12date{HH:mm:ss} %-5level %logger{35} - %msg%n'
@@ -31,4 +47,3 @@ logger 'org.apache.tomcat', ERROR
 logger 'org.apache.coyote', ERROR
 logger 'org.apache.catalina', ERROR
 logger 'org.apache.jasper', ERROR
-
