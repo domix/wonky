228e3f7e3566a8258d6591994e5d4cb121086354
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-09-09 17:13:30 -0500

    upgraded libs

--- src/main/groovy/com/domingosuarez/wonky/service/MessageService.groovy
+++ src/main/groovy/com/domingosuarez/wonky/service/MessageService.groovy
@@ -22,13 +22,11 @@ import static org.springframework.context.i18n.LocaleContextHolder.locale
 import com.domingosuarez.boot.autoconfigure.jade4j.JadeHelper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.MessageSource
-import org.springframework.stereotype.Component
 
 /**
  * Created by domix on 26/08/15.
  */
-@JadeHelper
-@Component('message')
+@JadeHelper('message')
 class MessageService {
   @Autowired
   MessageSource messageSource
