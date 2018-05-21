a9a1bd17fc2a911acc754d30c106f8d12e6279e0
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-05-21 16:55:11 -0500

    domain class

--- src/main/java/wonky/service/SlackOrganization.java
+++ src/main/java/wonky/service/SlackOrganization.java
@@ -1,5 +1,12 @@
 package wonky.service;
 
-public class SlackOrganization {
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+public class SlackOrganization {
+  private String teamDomain;
+  private String token;
+  private String wonkyDomain;
 }
