d2c22741c45e54c7bb6b6016027e9a4c8b5ca230
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-05-29 19:27:50 -0500

    implemented the auto realing when the tenants file changes

--- src/main/java/wonky/api/ApiController.java
+++ src/main/java/wonky/api/ApiController.java
@@ -2,14 +2,22 @@ package wonky.api;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import wonky.service.SlackOrganization;
+import wonky.service.SlackService;
+
+import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Created by domix on 01/06/18.
  */
 @Controller("/hello")
 public class ApiController {
+  @Inject
+  private SlackService slackService;
+
   @Get("/")
-  public String index() {
-    return "Hello World";
+  public List<SlackOrganization> index() {
+    return slackService.getOrgs();
   }
 }
--- src/main/java/wonky/service/SlackService.java
+++ src/main/java/wonky/service/SlackService.java
@@ -1,15 +1,17 @@
 package wonky.service;
 
 import io.micronaut.context.annotation.Value;
+import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.monitor.FileAlterationListener;
+import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static java.lang.String.format;
@@ -18,30 +20,69 @@ import static java.lang.String.format;
  * Created by domix on 01/06/18.
  */
 @Singleton
-@Setter
-@Slf4j
 public class SlackService {
+  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SlackService.class);
   @Value("${wonky.tenants.file:/etc/wonky/tenants.yaml}")
+  @Setter
+  @Getter
   private String tenantsFile;
+
+  @Value("${wonky.tenants.file.pollinterval:100}")
+  private int POLL_INTERVAL = 100;
 
   private List<SlackOrganization> orgs;
 
   @PostConstruct
+  public void init() {
+    File file = new File(tenantsFile);
+    String tenantsFileDirectory = file.getParentFile().getAbsolutePath();
+    log.info("Watching changes in [{}]", tenantsFileDirectory);
+    log.info("Poll interval [{}]", POLL_INTERVAL);
+    FileAlterationObserver observer = new FileAlterationObserver(tenantsFileDirectory);
+    FileAlterationMonitor monitor = new FileAlterationMonitor(POLL_INTERVAL);
+
+    load();
+
+    FileAlterationListener listener = new FileAlterationListenerAdaptor() {
+      @Override
+      public void onFileChange(File file) {
+        log.debug("onFileChange");
+        log.info("Reloading file [{}]", file.getAbsoluteFile().getName());
+        load();
+      }
+    };
+    observer.addListener(listener);
+    monitor.addObserver(observer);
+    try {
+      monitor.start();
+    } catch (Exception e) {
+      throw new IllegalStateException(e.getMessage(), e);
+    }
+  }
+
   public void load() {
+    log.info("Loading [{}]", tenantsFile);
     Yaml yaml = new Yaml();
 
     try {
       File file = new File(tenantsFile);
       InputStream ios = new FileInputStream(file);
       orgs = yaml.load(ios);
-      String tenantsFileDirectory = file.getParentFile().getName();
-      Path path = Paths.get(tenantsFileDirectory);
+
       ios.close();
     } catch (FileNotFoundException e) {
-      //log.error(format("Can't load tenants file. '%s'", tenantsFile), e);
+      log.error(format("Can't load tenants file. '%s'", tenantsFile), e);
       throw new IllegalStateException(format("Can't load tenants file. '%s'", tenantsFile), e);
     } catch (IOException e) {
-      //log.warn(e.getMessage(), e);
+      log.warn(e.getMessage(), e);
     }
+  }
+
+  public List<SlackOrganization> getOrgs() {
+    return orgs;
+  }
+
+  public void setOrgs(List<SlackOrganization> orgs) {
+    this.orgs = orgs;
   }
 }
