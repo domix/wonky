4883cd2712bb33158120012896dce5a584ab841b
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-06-13 17:27:25 -0500

    Using Lombok

--- src/main/java/wonky/slack/Team.java
+++ src/main/java/wonky/slack/Team.java
@@ -1,11 +1,16 @@
 package wonky.slack;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import wonky.slack.Icon;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Created by domix on 05/06/18.
  */
+@Setter
+@Getter
+@ToString
 public class Team {
   private String domain;
   @JsonProperty("email_domain")
@@ -13,55 +18,4 @@ public class Team {
   private Icon icon;
   private String id;
   private String name;
-
-  @Override
-  public String toString() {
-    return "Team{" +
-      "domain='" + domain + '\'' +
-      ", emailDomain='" + emailDomain + '\'' +
-      ", icon=" + icon +
-      ", id='" + id + '\'' +
-      ", name='" + name + '\'' +
-      '}';
-  }
-
-  public String getDomain() {
-    return domain;
-  }
-
-  public void setDomain(String domain) {
-    this.domain = domain;
-  }
-
-  public String getEmailDomain() {
-    return emailDomain;
-  }
-
-  public void setEmailDomain(String emailDomain) {
-    this.emailDomain = emailDomain;
-  }
-
-  public Icon getIcon() {
-    return icon;
-  }
-
-  public void setIcon(Icon icon) {
-    this.icon = icon;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 }
