de6c1835ec83d55f3c0168fae9ec9747f00af7ab
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-06-16 17:33:25 -0500

    Using Lombok

--- src/main/java/wonky/model/Organization.java
+++ src/main/java/wonky/model/Organization.java
@@ -1,17 +1,15 @@
 package wonky.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import wonky.slack.Team;
 
+@Setter
+@Getter
+@ToString
 public class Organization {
   private Team team;
   //TODO: User info
   //TODO: channels info
-
-  public Team getTeam() {
-    return team;
-  }
-
-  public void setTeam(Team team) {
-    this.team = team;
-  }
 }
