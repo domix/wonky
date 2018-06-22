67d6bfe7422a4e8bedf05d1fa2da60f01084a892
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-06-22 17:44:35 -0500

    Using Lombok

--- src/main/java/wonky/api/ApiController.java
+++ src/main/java/wonky/api/ApiController.java
@@ -3,6 +3,7 @@ package wonky.api;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Header;
+import lombok.extern.slf4j.Slf4j;
 import wonky.model.Organization;
 import wonky.service.SlackService;
 
@@ -11,9 +12,9 @@ import javax.inject.Inject;
 /**
  * Created by domix on 01/06/18.
  */
+@Slf4j
 @Controller("/v1/organizations")
 public class ApiController {
-  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ApiController.class);
   @Inject
   private SlackService slackService;
 
