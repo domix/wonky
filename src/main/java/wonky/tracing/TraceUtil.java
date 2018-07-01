9d6c2f284beba6ef1057f2c3d54b94a427e39015
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-06-30 19:56:39 -0500

    initial impl

--- src/main/java/wonky/tracing/TraceUtil.java
+++ src/main/java/wonky/tracing/TraceUtil.java
@@ -1,4 +1,14 @@
 package wonky.tracing;
 
+import io.opentracing.Tracer;
+
+import javax.inject.Singleton;
+
+@Singleton
 public class TraceUtil {
+  private final Tracer tracer;
+
+  public TraceUtil(Tracer tracer) {
+    this.tracer = tracer;
+  }
 }
