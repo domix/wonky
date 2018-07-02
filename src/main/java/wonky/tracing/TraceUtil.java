7d9eee77b21f95e1fd78b204205aaec1754eceda
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-07-01 20:09:22 -0500

    wip trace util

--- src/main/java/wonky/tracing/TraceUtil.java
+++ src/main/java/wonky/tracing/TraceUtil.java
@@ -1,14 +1,28 @@
 package wonky.tracing;
 
+import io.opentracing.Span;
 import io.opentracing.Tracer;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Singleton;
 
 @Singleton
+@Slf4j
 public class TraceUtil {
   private final Tracer tracer;
 
   public TraceUtil(Tracer tracer) {
     this.tracer = tracer;
+  }
+
+  public <T> T trace(String content, Class<T> valueType) {
+    Span currentSpan = tracer.activeSpan();
+    Tracer.SpanBuilder dd = tracer.buildSpan("dd");
+    Span start = dd.withTag("hostname", "localhost").start();
+    log.info("Looking for [{}]", "localhost");
+
+    start.log("Calling done.");
+    start.finish();
+    return null;
   }
 }
