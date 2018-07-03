14f5fc72498efd1c7e251f89f4f6622076e4b47f
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-07-03 11:44:41 -0500

    Enhanced tracing

--- src/main/java/wonky/tracing/TraceUtil.java
+++ src/main/java/wonky/tracing/TraceUtil.java
@@ -1,11 +1,18 @@
 package wonky.tracing;
 
+import io.micronaut.tracing.interceptor.TraceInterceptor;
+import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static java.lang.String.format;
 
 @Singleton
 @Slf4j
@@ -17,14 +24,45 @@ public class TraceUtil {
     this.tracer = tracer;
   }
 
-  public <T> T trace(String content, Class<T> valueType) {
-    Span currentSpan = tracer.activeSpan();
-    Tracer.SpanBuilder dd = tracer.buildSpan("dd");
-    Span start = dd.withTag("hostname", "localhost").start();
-    log.info("Looking for [{}]", "localhost");
-
-    start.log("Calling done.");
-    start.finish();
+  public <T> T trace(Supplier<T> supplier) {
     return null;
+  }
+
+  public <T> T trace(Function<Span, T> function) {
+    StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+    int index = 1;
+    if (stackTraceElements.length > 2) {
+      index = 2;
+    }
+    StackTraceElement stackTraceElement = stackTraceElements[index];
+    String className = stackTraceElement.getClassName();
+    String methodName = stackTraceElement.getMethodName();
+    String lineNumber = String.valueOf(stackTraceElement.getLineNumber());
+
+    Span activeSpan = tracer.activeSpan();
+    String operationName = format("%s.%s", className, methodName);
+
+    Tracer.SpanBuilder builder = tracer.buildSpan(operationName);
+
+    if (!Objects.isNull(activeSpan)) {
+      builder.asChildOf(activeSpan);
+    }
+
+    try (Scope scope = builder.startActive(true)) {
+      Span span = scope.span();
+      span.setTag("className", className);
+      span.setTag("methodName", methodName);
+      //span.setTag("lineNumber", lineNumber);
+      span.log(format("%s:%s", operationName, lineNumber));
+
+      try {
+        return function.apply(span);
+      } catch (RuntimeException e) {
+        TraceInterceptor.logError(scope.span(), e);
+        throw e;
+      }
+    }
+
   }
 }
