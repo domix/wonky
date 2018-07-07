9b1fce773d3d9c56e5efc1dc13a58f7f1e5ec77e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-07-06 22:20:06 -0500

    removed unused code

--- src/main/java/wonky/service/SlackOrganization.java
+++ src/main/java/wonky/service/SlackOrganization.java
@@ -2,14 +2,12 @@ package wonky.service;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 /**
  * Created by domix on 01/06/18.
  */
 @Setter
 @Getter
-@ToString
 public class SlackOrganization {
   private String teamDomain;
   private String token;
--- src/main/java/wonky/slack/Team.java
+++ src/main/java/wonky/slack/Team.java
@@ -3,14 +3,12 @@ package wonky.slack;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 /**
  * Created by domix on 05/06/18.
  */
 @Setter
 @Getter
-@ToString
 public class Team {
   private String domain;
   @JsonProperty("email_domain")
