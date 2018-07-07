e5919d409de31d4beb617e12eb1f2f2f0dfa156b
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-07-06 22:20:06 -0500

    removed unused code

--- src/main/java/wonky/model/Organization.java
+++ src/main/java/wonky/model/Organization.java
@@ -2,12 +2,10 @@ package wonky.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import wonky.slack.Team;
 
 @Setter
 @Getter
-@ToString
 public class Organization {
   private Team team;
   //TODO: User info
--- src/main/java/wonky/service/WonkyOrganizations.java
+++
@@ -1,7 +0,0 @@
-package wonky.service;
-
-import java.util.List;
-
-public class WonkyOrganizations {
-  private List<SlackOrganization> organizations;
-}
