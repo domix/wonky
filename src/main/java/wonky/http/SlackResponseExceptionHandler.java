0a3acddf855b9e7ec89799c4c94a0622e504c92f
Domingo Suarez Torres <domingo.suarez@gmail.com>
2018-12-27 21:26:35 -0600

    code cleanup

--- src/main/java/wonky/http/SlackResponseExceptionHandler.java
+++ src/main/java/wonky/http/SlackResponseExceptionHandler.java
@@ -1,7 +1,6 @@
 package wonky.http;
 
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Produces;
@@ -10,6 +9,8 @@ import io.micronaut.http.server.exceptions.ExceptionHandler;
 import javax.inject.Singleton;
 import java.util.Map;
 
+import static io.micronaut.core.util.CollectionUtils.mapOf;
+
 @Produces
 @Singleton
 @Requires(classes = {SlackResponseException.class, ExceptionHandler.class})
@@ -17,11 +18,10 @@ public class SlackResponseExceptionHandler implements ExceptionHandler<SlackResp
 
   @Override
   public HttpResponse handle(HttpRequest request, SlackResponseException exception) {
-    Map data = CollectionUtils
-      .mapOf(
-        "message", exception.getMessage(),
-        "error", exception.getError(),
-        "slackResponse", exception.getSlackResponse());
+    Map data = mapOf(
+      "message", exception.getMessage(),
+      "error", exception.getError(),
+      "slackResponse", exception.getSlackResponse());
 
     return HttpResponse.serverError(data);
   }
