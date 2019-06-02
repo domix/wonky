dba9d73ac959f9c98a3084fc656b06d8a3b06895
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-06-02 12:16:06 -0500

    code cleanup

--- src/main/java/wonky/security/AuthenticationProviderUserPassword.java
+++ src/main/java/wonky/security/AuthenticationProviderUserPassword.java
@@ -23,13 +23,13 @@ import org.reactivestreams.Publisher;
 import javax.inject.Singleton;
 import java.util.ArrayList;
 
-@Singleton
-public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+//@Singleton
+public class AuthenticationProviderUserPassword {/*implements AuthenticationProvider {
   @Override
   public Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
     if (authenticationRequest.getIdentity().equals("user") && authenticationRequest.getSecret().equals("password")) {
       return Flowable.just(new UserDetails("user", new ArrayList<>()));
     }
     return Flowable.just(new AuthenticationFailed());
-  }
+  }*/
 }
