5a6f09e1ed75530a22c9ba4845457206ebbc3911
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-06-03 12:18:22 -0500

    code cleanup

--- src/main/java/wonky/model/TelegramInfo.java
+++ src/main/java/wonky/model/TelegramInfo.java
@@ -16,13 +16,13 @@
  */
 package wonky.model;
 
-import io.micronaut.core.annotation.Introspected;
+/*import io.micronaut.core.annotation.Introspected;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@Introspected
+@Introspected*/
 public class TelegramInfo {
   private boolean activated;
   private boolean showChannels;
